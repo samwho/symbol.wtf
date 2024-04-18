e7952e449d09b49668f64bc1e9c8e4edb39a07a4
CatalanCabbage <dav.is@zohomail.in>
2024-04-18 15:07:52 +0530

    Add search bar, split symbols out

---
+++ symbols.js
@@ -0,0 +1,142 @@
+const symbols = [
+    {
+        "glyph": "©",
+        "name": "Copyright",
+        "searchTerms": ["copyright"]
+    },
+    {
+        "glyph": "®",
+        "name": "Registered Trademark",
+        "searchTerms": ["registered trademark"]
+    },
+    {
+        "glyph": "™",
+        "name": "Trademark",
+        "searchTerms": ["trademark"]
+    },
+    {
+        "glyph": "“",
+        "name": "Left Double Quotation Mark",
+        "searchTerms": ["quotation", "quote", "double"]
+    },
+    {
+        "glyph": "”",
+        "name": "Right Double Quotation Mark",
+        "searchTerms": ["quotation", "quote", "double"]
+    },
+    {
+        "glyph": "£",
+        "name": "Pound",
+        "searchTerms": ["pound"]
+    },
+    {
+        "glyph": "…",
+        "name": "Ellipsis",
+        "searchTerms": ["ellipsis"]
+    },
+    {
+        "glyph": "½",
+        "name": "Half",
+        "searchTerms": ["half"]
+    },
+    {
+        "glyph": "¼",
+        "name": "Quarter",
+        "searchTerms": ["quarter"]
+    },
+    {
+        "glyph": "∞",
+        "name": "Infinity",
+        "searchTerms": ["infinity"]
+    },
+    {
+        "glyph": "é",
+        "name": "E with Acute",
+        "searchTerms": ["acute", "e"]
+    },
+    {
+        "glyph": "á",
+        "name": "A with Acute",
+        "searchTerms": ["acute", "a"]
+    },
+    {
+        "glyph": "à",
+        "name": "A with Grave",
+        "searchTerms": ["grave", "a"]
+    },
+    {
+        "glyph": "ç",
+        "name": "C with Cedilla",
+        "searchTerms": ["cedilla", "c"]
+    },
+    {
+        "glyph": "€",
+        "name": "Euro",
+        "searchTerms": ["euro"]
+    },
+    {
+        "glyph": "¥",
+        "name": "Yen",
+        "searchTerms": ["yen"]
+    },
+    {
+        "glyph": "₩",
+        "name": "Won",
+        "searchTerms": ["won"]
+    },
+    {
+        "glyph": "¢",
+        "name": "Cent",
+        "searchTerms": ["cent"]
+    },
+    {
+        "glyph": "¤",
+        "name": "Currency",
+        "searchTerms": ["currency"]
+    },
+    {
+        "glyph": "œ",
+        "name": "OE",
+        "searchTerms": ["oe"]
+    },
+    {
+        "glyph": "Œ",
+        "name": "OE",
+        "searchTerms": ["oe"]
+    },
+    {
+        "glyph": "æ",
+        "name": "AE",
+        "searchTerms": ["ae"]
+    },
+    {
+        "glyph": "Æ",
+        "name": "AE",
+        "searchTerms": ["ae"]
+    },
+    {
+        "glyph": "✔",
+        "name": "Check",
+        "searchTerms": ["check", "tick"]
+    },
+    {
+        "glyph": "°",
+        "name": "Degree",
+        "searchTerms": ["degree"]
+    },
+    {
+        "glyph": "²",
+        "name": "Squared",
+        "searchTerms": ["squared", "power"]
+    },
+    {
+        "glyph": "³",
+        "name": "Cubed",
+        "searchTerms": ["cubed"]
+    },
+    {
+        "glyph": "‽",
+        "name": "Interrobang",
+        "searchTerms": ["interrobang", "?!"]
+    }
+]
