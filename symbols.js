d2d05b022b8130c4c048d5874cc98d0771542415
Navendu Pottekkat <navendu@apache.org>
2024-04-19 08:43:46 +0530

    Add Indian Rupee symbol (₹)
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>

--- symbols.js
+++ symbols.js
@@ -105,6 +105,11 @@ const symbols = [
         "searchTerms": ["won"]
     },
     {
+        "glyph": "₹",
+        "name": "Rupee",
+        "searchTerms": ["rupee"]
+    },
+    {
         "glyph": "¢",
         "name": "Cent",
         "searchTerms": ["cent"]
