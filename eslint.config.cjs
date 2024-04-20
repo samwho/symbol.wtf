edb38c51481b7cf163858aaaf71a9eefe3d51d06
Jason Parker <north@ntbox.com>
2024-04-20 17:58:45 -0400

    Add (broken in VS Code) HTML, CSS, and JSONC eslint plugins.

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,11 +2,30 @@ const globals = require("globals");
 const js = require("@eslint/js");
 const prettierRecommended = require("eslint-plugin-prettier/recommended");
 const prettierConfig = require("eslint-config-prettier");
+//const JSONCPlugin = require("eslint-plugin-jsonc");
+//const cssPlugin = require("eslint-plugin-css");
+const htmlPlugin = require("@html-eslint/eslint-plugin");
+const htmlParser = require("@html-eslint/parser");
 
 module.exports = [
 	js.configs.recommended,
 	prettierRecommended,
 	prettierConfig,
+	/* Crashes VS Code ESLint extension */
+	//...JSONCPlugin.configs["flat/recommended-with-jsonc"],
+	//...JSONCPlugin.configs["flat/prettier"],
+	/* Doesn't yet support flat config format */
+	//cssPlugin.configs.recommended,
+	{
+		files: ["**/*.html"],
+		plugins: { "@html-eslint": htmlPlugin },
+		languageOptions: {
+			parser: htmlParser,
+		},
+		rules: {
+			"@html-eslint/indent": "error",
+		},
+	},
 	{
 		files: ["**/*.js"],
 		languageOptions: {
