eb5f45936c0650c1cf93c7852d804269fdd95aa3
Jason Parker <north@ntbox.com>
2024-04-20 13:59:07 -0400

    Remove quotes from big array, remove empty searchTerms

--- symbols.js
+++ symbols.js
@@ -3,46 +3,45 @@ const DISPLAY_BOX = "\u25A1";
 const symbols = [
     /* samwho */
     {
-        "glyph": "©",
-        "name": "Copyright",
-        "searchTerms": ["(C)"]
+        glyph: "©",
+        name: "Copyright",
+        searchTerms: ["(C)"]
     },
     {
-        "glyph": "®",
-        "name": "Registered Trademark",
-        "searchTerms": ["(R)"]
+        glyph: "®",
+        name: "Registered Trademark",
+        searchTerms: ["(R)"]
     },
     {
-        "glyph": "™",
-        "name": "Trademark",
-        "searchTerms": ["TM"]
+        glyph: "™",
+        name: "Trademark",
+        searchTerms: ["TM"]
     },
 
     /* punctuation */
     {
-        "glyph": "“",
-        "name": "Left Double Quotation Mark",
-        "searchTerms": ["open", "quote", '"']
+        glyph: "“",
+        name: "Left Double Quotation Mark",
+        searchTerms: ["open", "quote", '"']
     },
     {
-        "glyph": "”",
-        "name": "Right Double Quotation Mark",
-        "searchTerms": ["close", "quote", '"']
+        glyph: "”",
+        name: "Right Double Quotation Mark",
+        searchTerms: ["close", "quote", '"']
     },
     {
-        "glyph": "‘",
-        "name": "Left Single Quotation Mark",
-        "searchTerms": ["open", "quote", "'"]
+        glyph: "‘",
+        name: "Left Single Quotation Mark",
+        searchTerms: ["open", "quote", "'"]
     },
     {
-        "glyph": "’",
-        "name": "Right Single Quotation Mark",
-        "searchTerms": ["close", "quote", "'"]
+        glyph: "’",
+        name: "Right Single Quotation Mark",
+        searchTerms: ["close", "quote", "'"]
     },
     {
-        "glyph": "—",
-        "name": "Em-dash",
-        "searchTerms": []
+        glyph: "—",
+        name: "Em-dash"
     },
     {
         glyph: "–",
@@ -50,327 +49,293 @@ const symbols = [
         searchTerms: ["en", "dash"]
     },
     {
-        "glyph": "¡",
-        "name": "Inverted Exclamation Mark",
-        "searchTerms": ["!"]
+        glyph: "¡",
+        name: "Inverted Exclamation Mark",
+        searchTerms: ["!"]
     },
     {
-        "glyph": "¿",
-        "name": "Inverted Question Mark",
-        "searchTerms": ["?"]
+        glyph: "¿",
+        name: "Inverted Question Mark",
+        searchTerms: ["?"]
     },
     {
-        "glyph": "‽",
-        "name": "Interrobang",
-        "searchTerms": ["?!"]
+        glyph: "‽",
+        name: "Interrobang",
+        searchTerms: ["?!"]
     },
     {
-        "glyph": "…",
-        "name": "Ellipsis",
-        "searchTerms": ["..."]
+        glyph: "…",
+        name: "Ellipsis",
+        searchTerms: ["..."]
     },
     {
-        "glyph": "•",
-        "name": "Vertically Centered Dot",
-        "searchTerms": ["bullet", "."]
+        glyph: "•",
+        name: "Vertically Centered Dot",
+        searchTerms: ["bullet", "."]
     },
     {
-        "glyph": "§",
-        "name": "Section",
-        "searchTerms": ["silcrow", "s"]
+        glyph: "§",
+        name: "Section",
+        searchTerms: ["silcrow", "s"]
     },
     {
-        "glyph": "¶",
-        "name": "Paragraph",
-        "searchTerms": ["pilcrow", "p"]
+        glyph: "¶",
+        name: "Paragraph",
+        searchTerms: ["pilcrow", "p"]
     },
 
     /* currency */
     {
-        "glyph": "¤",
-        "name": "Currency",
-        "searchTerms": []
+        glyph: "¤",
+        name: "Currency"
     },
     {
-        "glyph": "£",
-        "name": "Pound",
-        "searchTerms": []
+        glyph: "£",
+        name: "Pound"
     },
     {
-        "glyph": "€",
-        "name": "Euro",
-        "searchTerms": []
+        glyph: "€",
+        name: "Euro"
     },
     {
-        "glyph": "$",
-        "name": "Dollar Sign",
-        "searchTerms": []
+        glyph: "$",
+        name: "Dollar Sign"
     },
     {
-        "glyph": "¥",
-        "name": "Yen",
-        "searchTerms": []
+        glyph: "¥",
+        name: "Yen"
     },
     {
-        "glyph": "₩",
-        "name": "Won",
-        "searchTerms": []
+        glyph: "₩",
+        name: "Won"
     },
     {
         glyph: "₹",
-        name: "Rupee",
-        searchTerms: []
+        name: "Rupee"
     },
     {
-        "glyph": "¢",
-        "name": "Cent",
-        "searchTerms": []
+        glyph: "¢",
+        name: "Cent"
     },
 
     /* math */
     {
-        "glyph": "±",
-        "name": "Plus-minus",
-        "searchTerms": ["+", "-"]
+        glyph: "±",
+        name: "Plus-minus",
+        searchTerms: ["+", "-"]
     },
     {
-        "glyph": "×",
-        "name": "Times",
-        "searchTerms": ["multiply", "*", "x"]
+        glyph: "×",
+        name: "Times",
+        searchTerms: ["multiply", "*", "x"]
     },
     {
-        "glyph": "÷",
-        "name": "Divide",
-        "searchTerms": ["/"]
+        glyph: "÷",
+        name: "Divide",
+        searchTerms: ["/"]
     },
     {
-        "glyph": "√",
-        "name": "Square Root",
-        "searchTerms": ["sqrt"]
+        glyph: "√",
+        name: "Square Root",
+        searchTerms: ["sqrt"]
     },
     {
-        "glyph": "∑",
-        "name": "Summation",
-        "searchTerms": ["sum"]
+        glyph: "∑",
+        name: "Summation",
+        searchTerms: ["sum"]
     },
     {
-        "glyph": "∏",
-        "name": "Product",
-        "searchTerms": []
+        glyph: "∏",
+        name: "Product"
     },
     {
-        "glyph": "∫",
-        "name": "Integral",
-        "searchTerms": []
+        glyph: "∫",
+        name: "Integral"
     },
     {
-        "glyph": "∂",
-        "name": "Partial Derivative",
-        "searchTerms": []
+        glyph: "∂",
+        name: "Partial Derivative"
     },
     {
-        "glyph": "¹",
-        "name": "Superscript One",
-        "searchTerms": ["power"]
+        glyph: "¹",
+        name: "Superscript One",
+        searchTerms: ["power"]
     },
     {
-        "glyph": "²",
-        "name": "Superscript Two",
-        "searchTerms": ["squared", "power"]
+        glyph: "²",
+        name: "Superscript Two",
+        searchTerms: ["squared", "power"]
     },
     {
-        "glyph": "³",
-        "name": "Superscript Three",
-        "searchTerms": ["cubed", "power"]
+        glyph: "³",
+        name: "Superscript Three",
+        searchTerms: ["cubed", "power"]
     },
     {
-        "glyph": "¼",
-        "name": "One Quarter",
-        "searchTerms": ["fraction", "1/4"]
+        glyph: "¼",
+        name: "One Quarter",
+        searchTerms: ["fraction", "1/4"]
     },
     {
-        "glyph": "½",
-        "name": "One Half",
-        "searchTerms": ["fraction", "1/2"]
+        glyph: "½",
+        name: "One Half",
+        searchTerms: ["fraction", "1/2"]
     },
     {
-        "glyph": "¾",
-        "name": "Three Quarters",
-        "searchTerms": ["fraction", "3/4"]
+        glyph: "¾",
+        name: "Three Quarters",
+        searchTerms: ["fraction", "3/4"]
     },
     {
-        "glyph": "∞",
-        "name": "Infinity",
-        "searchTerms": []
+        glyph: "∞",
+        name: "Infinity"
     },
     {
-        "glyph": "∅",
-        "name": "Empty Set",
-        "searchTerms": []
+        glyph: "∅",
+        name: "Empty Set"
     },
     {
-        "glyph": "π",
-        "name": "pi",
-        "searchTerms": ["pie"]
+        glyph: "π",
+        name: "pi",
+        searchTerms: ["pie"]
     },
     {
-        "glyph": "∆",
-        "name": "Delta",
-        "searchTerms": []
+        glyph: "∆",
+        name: "Delta"
     },
     {
-        "glyph": "ε",
-        "name": "Epsilon",
-        "searchTerms": []
+        glyph: "ε",
+        name: "Epsilon"
     },
     {
-        "glyph": "µ",
-        "name": "Micro",
-        "searchTerms": ["mu"]
+        glyph: "µ",
+        name: "Micro",
+        searchTerms: ["mu"]
     },
     {
-        "glyph": "°",
-        "name": "Degree",
-        "searchTerms": []
+        glyph: "°",
+        name: "Degree"
     },
 
     /* accented characters */
     {
-        "glyph": "á",
-        "name": "A with Acute",
-        "searchTerms": []
+        glyph: "á",
+        name: "A with Acute"
     },
     {
-        "glyph": "à",
-        "name": "A with Grave",
-        "searchTerms": []
+        glyph: "à",
+        name: "A with Grave"
     },
     {
-        "glyph": "ç",
-        "name": "C with Cedilla",
-        "searchTerms": []
+        glyph: "ç",
+        name: "C with Cedilla"
     },
     {
-        "glyph": "é",
-        "name": "E with Acute",
-        "searchTerms": []
+        glyph: "é",
+        name: "E with Acute"
     },
     {
-        "glyph": "ñ",
-        "name": "Latin Small Letter n with Tilde",
-        "searchTerms": ["jalapeno"]
+        glyph: "ñ",
+        name: "Latin Small Letter n with Tilde",
+        searchTerms: ["jalapeno"]
     },
     {
-        "glyph": "Ñ",
-        "name": "Latin Capital Letter N with Tilde",
-        "searchTerms": ["jalapeno"]
+        glyph: "Ñ",
+        name: "Latin Capital Letter N with Tilde",
+        searchTerms: ["jalapeno"]
     },
 
     /* combined characters */
     {
-        "glyph": "æ",
-        "name": "AE",
-        "searchTerms": []
+        glyph: "æ",
+        name: "AE"
     },
     {
-        "glyph": "Æ",
-        "name": "AE",
-        "searchTerms": []
+        glyph: "Æ",
+        name: "AE"
     },
     {
-        "glyph": "œ",
-        "name": "OE",
-        "searchTerms": []
+        glyph: "œ",
+        name: "OE"
     },
     {
-        "glyph": "Œ",
-        "name": "OE",
-        "searchTerms": []
+        glyph: "Œ",
+        name: "OE"
     },
 
     /* miscellaneous */
     {
-        "glyph": "✔",
-        "name": "Check",
-        "searchTerms": ["tick"]
+        glyph: "✔",
+        name: "Check",
+        searchTerms: ["tick"]
     },
     {
-        "glyph": "←",
-        "name": "Left Arrow",
-        "searchTerms": []
+        glyph: "←",
+        name: "Left Arrow"
     },
     {
-        "glyph": "→",
-        "name": "Right Arrow",
-        "searchTerms": []
+        glyph: "→",
+        name: "Right Arrow"
     },
     {
         glyph: "↑",
-        name: "Upwards Arrow",
-        searchTerms: []
+        name: "Upwards Arrow"
     },
     {
         glyph: "↓",
-        name: "Downwards Arrow",
-        searchTerms: []
+        name: "Downwards Arrow"
     },
     {
         glyph: "↔",
-        name: "Left Right Arrow",
-        searchTerms: []
+        name: "Left Right Arrow"
     },
     {
         glyph: "↕",
-        name: "Up Down Arrow",
-        searchTerms: []
+        name: "Up Down Arrow"
     },
     {
-        "glyph": "~",
-        "name": "Tilde",
-        "searchTerms": []
+        glyph: "~",
+        name: "Tilde"
     },
     {
-        "glyph": "⌘",
-        "name": "Command (Looped Square)",
-        "searchTerms": []
+        glyph: "⌘",
+        name: "Command (Looped Square)"
     },
     {
-        "glyph": "⌥",
-        "name": "Option",
-        "searchTerms": []
+        glyph: "⌥",
+        name: "Option"
     },
 
     /* invisible characters */
     {
-        "glyph": "\u00A0",
-        "display": DISPLAY_BOX,
-        "name": "No-break Space",
-        "searchTerms": ["&nbsp;", "non-breaking"]
+        glyph: "\u00A0",
+        display: DISPLAY_BOX,
+        name: "No-break Space",
+        searchTerms: ["&nbsp;", "non-breaking"]
     },
     {
-        "glyph": "\u200B",
-        "display": DISPLAY_BOX,
-        "name": "Zero Width Space",
-        "searchTerms": ["zwsp"]
+        glyph: "\u200B",
+        display: DISPLAY_BOX,
+        name: "Zero Width Space",
+        searchTerms: ["zwsp"]
     },
     {
-        "glyph": "\u200E",
-        "display": DISPLAY_BOX,
-        "name": "Left-to-Right",
-        "searchTerms": ["&lrm;", "ltr"]
+        glyph: "\u200E",
+        display: DISPLAY_BOX,
+        name: "Left-to-Right",
+        searchTerms: ["&lrm;", "ltr"]
     },
     {
-        "glyph": "\u200F",
-        "display": DISPLAY_BOX,
-        "name": "Right-to-Left",
-        "searchTerms": ["&rlm;", "rtl"]
+        glyph: "\u200F",
+        display: DISPLAY_BOX,
+        name: "Right-to-Left",
+        searchTerms: ["&rlm;", "rtl"]
     },
 
     /* Private Use Area (not official Unicode, may not display) */
     {
-        "glyph": "\uF8FF",
-        "name": "Apple Logo",
-        "searchTerms": []
+        glyph: "\uF8FF",
+        name: "Apple Logo"
     }
 ];
 
